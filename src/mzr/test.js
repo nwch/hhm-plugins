f72623c2b1cccd20e7186d7da1c283e7542c5beb
adam <adam@dakrysia.eu>
2021-02-14 13:44:02 +0100

    feat: :art: send chat message

--- src/mzr/test.js
+++ src/mzr/test.js
@@ -5,11 +5,12 @@ room.pluginSpec = {
     author: `mzr`,
     version: `1.0.0`,
      dependencies: [
-         `sav/cron`
+         `sav/cron`,
+         `sav/chat`,
     ],
     // config: {
     //     text: `default message`
     // }
 };
 
-//room.onCron10GameSeconds = () => room.sendChat("10 ingame seconds have passed");
+room.onCron10GameSeconds = () => room.sendChat("10 ingame seconds have passed");
