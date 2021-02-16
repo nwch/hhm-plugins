a5784f5adaf3298d19344e801f2654f56bf36027
adam <adam@dakrysia.eu>
2021-02-16 20:19:01 +0100

    fix: :art: dsfsdfsdfs

--- src/mzr/test.js
+++ src/mzr/test.js
@@ -3,12 +3,12 @@ let room = HBInit();
 room.pluginSpec = {
     name: `mzr/test`,
     author: `mzr`,
-    version: `1.0.1`,
-     dependencies: [
-         //`sav/cron`,
-         `sav/chat`,
-         `sav/players`,
-    ],
+    version: `1.1.2`,
+    //  dependencies: [
+    //      //`sav/cron`,
+    //      `sav/chat`,
+    //      //`sav/players`,
+    // ],
     // config: {
     //     text: `default message`
     // }
@@ -18,7 +18,7 @@ room.pluginSpec = {
 //     this.sendChat(player.name + ' joined hahaha')
 // });
 
-room.onPlayerJoin = (player) => room.sendChat("game started NOW");
+room.onPlayerJoin = (player) => room.sendChat("game started NOW!");
 // room.onCron10GameSeconds = () => room.sendChat("10 ingame seconds have passed");
 
 // function onPlayerJoin(player) {
