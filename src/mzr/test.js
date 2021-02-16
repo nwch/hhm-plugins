58f1774920f3701a1ae1a1f26bba683a34b7698a
adam <adam@dakrysia.eu>
2021-02-16 20:14:56 +0100

    feat: :bug: first step

--- src/mzr/test.js
+++ src/mzr/test.js
@@ -5,7 +5,7 @@ room.pluginSpec = {
     author: `mzr`,
     version: `1.0.1`,
      dependencies: [
-         `sav/cron`,
+         //`sav/cron`,
          `sav/chat`,
          `sav/players`,
     ],
@@ -18,18 +18,18 @@ room.pluginSpec = {
 //     this.sendChat(player.name + ' joined hahaha')
 // });
 
-// room.onPlayerJoin = (player) => room.sendChat("game started NOW");
+room.onPlayerJoin = (player) => room.sendChat("game started NOW");
 // room.onCron10GameSeconds = () => room.sendChat("10 ingame seconds have passed");
 
-function onPlayerJoin(player) {
-    room.sendChat(`player moved to red`);
-    room.sendAnnouncement(`off we go`, player.id, { prefix: [`CMD`] });
-    room.setPlayerTeam(player.id, 1);
-    // room.sendChat(`zonk?`);
-}
+// function onPlayerJoin(player) {
+//     room.sendChat(`player moved to red`);
+//     room.sendAnnouncement(`off we go`, player.id, { prefix: [`CMD`] });
+//     room.setPlayerTeam(player.id, 1);
+//     // room.sendChat(`zonk?`);
+// }
 
-room.onPlayerJoin = onPlayerJoin;
-room.onGameStart = () => room.sendChat(`global zonk?!`);
+// room.onPlayerJoin = onPlayerJoin;
+// room.onGameStart = () => room.sendChat(`global zonk?!`);
 
 // room.onRoomLink = function onRoomLink() {
     
