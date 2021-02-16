af90560b309f540e9622c4bdec7caff7212ae3bb
adam <adam@dakrysia.eu>
2021-02-16 19:19:14 +0100

    feat: :zap: Game started info

--- src/mzr/test.js
+++ src/mzr/test.js
@@ -5,12 +5,18 @@ room.pluginSpec = {
     author: `mzr`,
     version: `1.0.0`,
      dependencies: [
-         `sav/cron`,
+         //`sav/cron`,
          `sav/chat`,
+         `sav/players`,
     ],
     // config: {
     //     text: `default message`
     // }
 };
 
-room.onCron10GameSeconds = () => room.sendChat("10 ingame seconds have passed");
+// room.on('player-join', function (player) {
+//     this.sendChat(player.name + ' joined hahaha')
+// });
+
+room.onPlayerJoin = () => room.sendChat("game started NOW");
+// room.onCron10GameSeconds = () => room.sendChat("10 ingame seconds have passed");
