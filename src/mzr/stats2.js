d5f0f32df8a7501df9f69d8da57b7363f28b4936
adam <adam@dakrysia.eu>
2021-02-17 20:16:36 +0100

    feat: :goal_net: goal scorers

---
+++ src/mzr/stats2.js
@@ -0,0 +1,82 @@
+let room = HBInit();
+
+room.pluginSpec = {
+    name: `mzr/stats2`,
+    author: `mzr`,
+    version: `1.0.0`,
+     dependencies: [
+         //`sav/cron`,
+        `sav/core`,
+        `sav/chat`,
+    ],
+    config: {}
+};
+
+let lastTouch = {
+    scorer: null,
+    assister: null,
+};
+
+function updateLastTouch(player) {
+    if (lastTouch.scorer !== null) {
+      if (player.id !== lastTouch.scorer.id) {
+        lastTouch.assister = lastTouch.scorer;
+        lastTouch.scorer = player;
+      }
+    } else {
+      lastTouch.scorer = player;
+    }
+}
+
+function onTeamGoalHandler(teamId) {
+    room.sendAnnouncement(addGoal(teamId));
+    lastTouch = {
+        scorer: null,
+        assister: null,
+    };
+};
+  
+function addGoal(teamId) {
+    scores = room.getScores();
+    output = '' + scores.red + '-' + scores.blue + ' - ';
+    if (lastTouch.scorer.team !== teamId) {
+      output = output + 'Own goal by: ' + lastTouch.scorer.name;
+    } else {
+      output = output + 'Goal by: ' + lastTouch.scorer.name;
+    }
+  
+    if (lastTouch.assister !== null) {
+      if (lastTouch.assister.team === teamId) {
+        output = output + ', Assist by: ' + lastTouch.assister.name;
+      }
+    }
+
+    return output;
+}
+
+function onGameStartHandler() {
+    lastTouch = {
+        scorer: null,
+        assister: null,
+    };
+}
+
+function onBallKickHandler(player) {
+    updateLastTouch(player);
+    //room.sendAnnouncement("He kicked the ball :o");
+    // if (!goalScored) {
+    //     pass = player;
+    //     checkPass(player);
+    //     addPossession(player.id);
+    //     updateLastTouch(player);
+    //     possBuffer = 0;
+    // }
+}
+
+room.onRoomLink = function onRoomLink() {
+    room.onPlayerBallKick = onBallKickHandler;
+    room.onGameStart = onGameStartHandler;
+    room.onTeamGoal = onTeamGoalHandler;
+    // room.onPlayerJoin = onPlayerJoinHandler;
+    // room.onPlayerLeave = onPlayerLeaveHandler;
+}
