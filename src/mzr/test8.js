e8a655e2539e51022f449fbbf1c31c27d08d6461
adam <adam@dakrysia.eu>
2021-02-17 14:20:48 +0100

    feat: :zap: add afk command

--- src/mzr/test8.js
+++ src/mzr/test8.js
@@ -1,13 +1,14 @@
 let room = HBInit();
 
 room.pluginSpec = {
-    name: `mzr/test7`,
+    name: `mzr/test8`,
     author: `mzr`,
     version: `1.0.0`,
      dependencies: [
          //`sav/cron`,
         `sav/chat`,
         `sav/players`,
+        `sav/commands`,
     ],
     config: {}
 };
@@ -43,19 +44,23 @@ function getPlayerListByTeam() {
     return results;
 }
 
-function onPlayerJoinHandler(player) {
-    room.sendChat("game started NOW7");
+function chooseTeam(player) {
     let teams = getPlayerListByTeam();
 
     let reds = teams[1];
     let blues = teams[2];
 
     if (blues.length > reds.length) {
-        joinBlue(player.id);
-    } else {        
         joinRed(player.id);
+    } else {        
+        joinBlue(player.id);
     }
+}
 
+function onPlayerJoinHandler(player) {
+    room.sendChat("game started NOW7");
+    
+    chooseTeam(player);
     // if (reds.length === blues.length) {
     //     if (lastDraw === 1) {
     //         joinBlue(player.id);
@@ -69,6 +74,21 @@ function onPlayerJoinHandler(player) {
     // } else {
     //     joinBlue(player.id);
     // }
+}
+
+room.onCommand_afk = {
+    function: (player) => {
+        if (player.team === 0) {
+            chooseTeam(player);
+        } else {
+            room.setPlayerTeam(player.id, 0);
+        }        
+    },
+    data: {
+        'sav/help': {
+            text: ' (move to spectators or back to one of the teams)'
+        }
+    }
 }
 
 room.onRoomLink = function onRoomLink() {
